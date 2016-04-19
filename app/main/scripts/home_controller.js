614f80f84a252cddd9921ed3c0f883161df3d868
PENGYUAN <pengyuan2015@u.northwestern.edu>
2016-04-19 15:00:20 -0500

    login1

--- app/main/scripts/home_controller.js
+++ app/main/scripts/home_controller.js
@@ -1,25 +1,30 @@
-angular
-  .module('main')
-  .controller('home_controller', function($scope, supersonic, $firebaseArray) {
-	var ref = new Firebase("https://hungryeinstein.firebaseio.com/");
+ main.controller('home_controller',
+	['$scope', 'Authentication', '$firebaseObject', 'FIREBASE_URL', '$firebaseArray', 'supersonic',
+	function($scope, Authentication, $firebaseObject, FIREBASE_URL, $firebaseArray, supersonic){
 
-	$scope.requests = $firebaseArray(ref);
+		var ref = new Firebase(FIREBASE_URL + 'users/');
+		// var syndata = $firebaseObject(ref);
+		// syndata.$bindTo($scope, "data");
 
-	$scope.addRequest = function(){
-	   console.log('Adding Request');
-	   $scope.requests.$add({
-	       name: $scope.name,
-	       subject: $scope.subject,
-	       expiry: $scope.expiry
-	   }).then(function(ref){
-	       var id = ref.key();
-	       console.log("Added Request" + id);
-	       $scope.name =" ";
-	       $scope.subject =" ";
-	       $scope.expiry =" ";
-	   });
-	}
-  });
+		$scope.messages = $firebaseArray(ref);
+
+		$scope.login = function() {
+			Authentication.login($scope.user);
+			
+		};
+		$scope.logout = function() {
+			Authentication.logout();
+		};
+		
+		$scope.register = function(){
+			Authentication.register($scope.user);
+		};
+
+		$scope.addRequest = function(){
+			Authentication.addRequest();
+		};
+
+}]);
 
 
 
