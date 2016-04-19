025191d91a72f90b6610513067df2ad1c0218e2d
nix-goat <waynexun2017@u.northwestern.edu>
2016-04-19 17:40:08 -0500

    got display working

--- dist/app/main.js
+++ dist/app/main.js
@@ -35,28 +35,50 @@ angular
 .controller('messages_controller', function($scope, supersonic, $firebaseArray) {
 	var ref = new Firebase("https://hungryeinstein.firebaseio.com/messages");
 
-	$scope.requests = $firebaseArray(ref);
+	updateMessages();
 
-	$scope.addRequest = function(){
-		console.log('Adding Request');
-			$scope.requests.$add({
+	$scope.addMessage = function(){
+		console.log('Sending Message');
+		$scope.messages.$add({
 			name: $scope.name,
 			message: $scope.message,
 		});
 	};
-	ref.on('child_added', function(snapshot) {
-		var message = snapshot.val();
-		displayChatMessage(message.name, message.message);
+	ref.on('child_added', function() {
+		updateMessages();
 	});
 
-	function displayChatMessage(name, message) {
-		supersonic.ui.dialog.alert(name + " >> " + message);
-	 	$scope.mything = $scope.mything +
-		 	"\n" + 
-		 		name + " >> " + message;
-	};
+	function updateMessages(){
+		$scope.messages = $firebaseArray(ref);
+	}
 
 });
+
+
+// Ariana's Code
+
+// main.factory('Authentication',
+//     ['$rootScope', '$firebaseAuth', 'FIREBASE_URL', '$location', '$firebaseObject', 'supersonic',
+//     function($rootScope, $firebaseAuth, FIREBASE_URL, $location, $firebaseObject, supersonic){
+
+        // var ref = new Firebase(FIREBASE_URL);
+        // var auth = $firebaseAuth(ref);
+//         // var temp_auth;           
+//        // successfully login and extract login user's infomation
+//         auth.$onAuth(function(authUser){
+//             if (authUser){
+//                 temp_auth = authUser;
+//                 var authRef = new Firebase(FIREBASE_URL + 'users/' + authUser.uid);
+//                 var userObj = $firebaseObject(authRef);
+//                 // $rootScope.messages =
+                
+//                 $rootScope.currentUser = userObj;
+
+
+//             } else {
+//                 $rootScope.currentUser = '';
+//             }
+//         });
 
 
 
