b666772d3c4b90729eb20116e83aae29b363e529
nix-goat <waynexun2017@u.northwestern.edu>
2016-04-19 16:24:30 -0500

    Rudimentary Firebase connection for messages, dedicated messages database. Can write can't read

--- app/main/scripts/home_controller.js
+++ app/main/scripts/home_controller.js
@@ -1,25 +1,25 @@
 angular
-  .module('main')
-  .controller('home_controller', function($scope, supersonic, $firebaseArray) {
+.module('main')
+.controller('home_controller', function($scope, supersonic, $firebaseArray) {
 	var ref = new Firebase("https://hungryeinstein.firebaseio.com/");
 
 	$scope.requests = $firebaseArray(ref);
 
 	$scope.addRequest = function(){
-	   console.log('Adding Request');
-	   $scope.requests.$add({
-	       name: $scope.name,
-	       subject: $scope.subject,
-	       expiry: $scope.expiry
-	   }).then(function(ref){
-	       var id = ref.key();
-	       console.log("Added Request" + id);
-	       $scope.name =" ";
-	       $scope.subject =" ";
-	       $scope.expiry =" ";
-	   });
+		console.log('Adding Request');
+		$scope.requests.$add({
+			name: $scope.name,
+			subject: $scope.subject,
+			expiry: $scope.expiry
+		}).then(function(ref){
+			var id = ref.key();
+			console.log("Added Request" + id);
+			$scope.name =" ";
+			$scope.subject =" ";
+			$scope.expiry =" ";
+		});
 	}
-  });
+});
 
 
 
--- app/main/scripts/messages_controller.js
+++ app/main/scripts/messages_controller.js
@@ -1,35 +1,3 @@
-angular.module('main', [
-  // Declare here all AngularJS dependencies that are shared by the example module.
-  'supersonic', 'firebase'
-]);
-	
-angular
-.module('main')
-.controller('home_controller', function($scope, supersonic, $firebaseArray) {
-	var ref = new Firebase("https://hungryeinstein.firebaseio.com/");
-
-	$scope.requests = $firebaseArray(ref);
-
-	$scope.addRequest = function(){
-		console.log('Adding Request');
-		$scope.requests.$add({
-			name: $scope.name,
-			subject: $scope.subject,
-			expiry: $scope.expiry
-		}).then(function(ref){
-			var id = ref.key();
-			console.log("Added Request" + id);
-			$scope.name =" ";
-			$scope.subject =" ";
-			$scope.expiry =" ";
-		});
-	}
-});
-
-
-
-
-
 angular
 .module('main')
 .controller('messages_controller', function($scope, supersonic, $firebaseArray) {
