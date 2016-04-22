25259d8eac2005649631931deda183aaabc83c52
nix-goat <waynexun2017@u.northwestern.edu>
2016-04-22 13:49:10 -0500

    front end

--- app/main/scripts/home_controller.js
+++ app/main/scripts/home_controller.js
@@ -19,7 +19,36 @@ angular
 			$scope.expiry =" ";
 		});
 	}
-});
+})
+
+ main.controller('home_controller',
+	['$scope', 'Authentication', '$firebaseObject', 'FIREBASE_URL', '$firebaseArray', 'supersonic',
+	function($scope, Authentication, $firebaseObject, FIREBASE_URL, $firebaseArray, supersonic){
+
+		var ref = new Firebase(FIREBASE_URL + 'users/');
+		// var syndata = $firebaseObject(ref);
+		// syndata.$bindTo($scope, "data");
+
+		$scope.messages = $firebaseArray(ref);
+
+		$scope.login = function() {
+			Authentication.login($scope.user);
+			
+		};
+		$scope.logout = function() {
+			Authentication.logout();
+		};
+		
+		$scope.register = function(){
+			Authentication.register($scope.user);
+		};
+
+		$scope.addRequest = function(){
+			Authentication.addRequest();
+		};
+
+}]);
+
 
 
 
