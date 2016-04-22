ec97817db7fe18f8e5ebaefdecfaba35408b4087
nix-goat <waynexun2017@u.northwestern.edu>
2016-04-22 01:45:43 -0500

    sending and conversation views fine, STILL cannot figure out message->conversation filtering...

--- dist/app/main.js
+++ dist/app/main.js
@@ -34,7 +34,8 @@ angular
 .module('main')
 .controller('messages_controller', function($scope, supersonic, $firebaseArray) {
 	var ref = new Firebase("https://hungryeinstein.firebaseio.com/messages");
-
+	// hardcoded for testing
+	var uID = "3333";
 	updateMessages();
 
 	$scope.addMessage = function(){
@@ -51,21 +52,100 @@ angular
 	});
 
 	function updateMessages(){
-		$scope.messages = $firebaseArray(ref);
+		$scope.messages = $firebaseArray(ref); //messArray;
+		$scope.messages.$loaded(function(x){
+			//supersonic.ui.dialog.alert("length is: " + $scope.messages.length);
+		});
+		// $scope.newmessages = makeToConvos($scope.messages);
+
+		// function makeToConvos(fbarray){
+		// 	//supersonic.ui.dialog.alert(String(fbarray.$keyAt(1)));
+		// 	return fbarray;
+		// }	
 	}
 
 	// when a conversation is selected,
 	// show that conversation
-	$scope.viewConversation = function(from, to, time){
-		steroids.logger.log("from: " + String(from) + ", to: " + String(to) + ", time: " + String(time));
+	$scope.viewConversation = function(item){
+		//steroids.logger.log("from: " + String(from) + ", to: " + String(to) + ", time: " + String(time));
 
-		var infoForView = {sender: from, receiver: to};
+		var infoForView = {otherPerson: getOtherPerson(item)};
 		var view = new supersonic.ui.View("main#view_conversation");
 		supersonic.ui.layers.push(view, {params: infoForView});
 		// opening views with options
 		// http://docs.appgyver.com/supersonic/api-reference/stable/supersonic/ui/layers/push/
+		
+		function getOtherPerson(item){
+		if (item.rcID == uID){
+			return item.sendID;
+		}
+		else if (item.sendID == uID){
+			return item.rcID;
+		}
+		else return 0;
+		}
 	}
 
+	// determine name of conversation
+	// input -> object with .rcID and .sendID
+	$scope.getTitle = function(item){
+		prefix = "conversation with ";
+		if (item.rcID == uID){
+			suffix = item.sendID;
+		}
+		else if (item.sendID == uID){
+			suffix = item.rcID;
+		}
+		return prefix + suffix;
+	}
+
+	// filter all messages for conversations
+	// all messages -> relevant messages -> unique conversations
+	$scope.convoFilter = function(item){
+		// $scope.convos.push(item);
+		//supersonic.ui.dialog.alert(convos.getIfIn(item));
+		//steroids.logger.log("item");
+		//convos.clear();
+		//steroids.logger.log(String("ifFirst"));
+		//supersonic.ui.dialog.alert(ifFirst);
+		// if (ifFirst){supersonic.ui.dialog.alert("here2");convos.clear();}
+		//if ($first){supersonic.ui.dialog.alert("first bitchsss");}
+		//if(ifStart){supersonic.ui.dialog.alert("first bitchsss");}
+		if (item.rcID == uID){return true;}
+		else if (item.sendID == uID){return true;}
+		return false;
+	}
+
+	// $scope.processMessages = function(messages){
+	// 	var convos = {
+	// 		list: [],
+	// 		getIfIn: function(id){
+	// 			for (i in this.list){
+	// 				if (this.list[i] == id){
+	// 				return true;
+	// 				}
+	// 			}
+	// 			this.list.push(id);
+	// 			//supersonic.ui.dialog.alert(this.list);
+	// 			return false;
+	// 		},
+	// 		clear: function(){this.list = [];},
+	// 	};
+
+	// 	toReturn = {};
+	// 	for (i in messages){
+	// 		if (messages[i].rcID == uID){
+	// 			toReturn.push(messages[i].sendID);
+	// 			supersonic.ui.dialog.alert("pushed sendID");
+	// 		}
+	// 		else if (messages[i].sendID == uID){
+	// 			toReturn.push(messages[i].rcID);
+	// 			supersonic.ui.dialog.alert("pushed rcID");
+	// 		}
+	// 	}
+
+	// 	return toReturn;
+	// }
 });
 
 
@@ -76,29 +156,42 @@ angular
 .module('main')
 .controller('view_conversation_controller', function($scope, supersonic, $firebaseArray) {
 	var ref = new Firebase("https://hungryeinstein.firebaseio.com/messages");
+	// hardcoded for testing
+	var uID = "3333";
 
 	updateMessages();
 
 	var passedData = {};
 	supersonic.ui.views.current.params.onValue(function(data){
 		passedData = data;
-	})
+		$scope.otherPerson = passedData.otherPerson;
+	});
 
 	$scope.addMessage = function(){
 		console.log('Sending Message');
 		$scope.messages.$add({
-			sendID: $scope.sendID,
-			rcID: $scope.rcID,
+			sendID: uID,
+			rcID: passedData.otherPerson,
 			text: $scope.text,
 			time: Firebase.ServerValue.TIMESTAMP,
 		});
 	};
 	ref.on('child_added', function() {
+		
 		updateMessages();
 	});
 
 	function updateMessages(){
+
 		$scope.messages = $firebaseArray(ref);
+	}
+
+	// determine if individual messages are part of the dialogue
+	$scope.dialogueFilter = function(item){
+		//supersonic.ui.dialog.alert("comparing " + String(item.rcID) + " and " + String(item.sendID));
+		if ((item.rcID == uID) && (item.sendID == passedData.otherPerson)){return true;}
+		if ((item.rcID == passedData.otherPerson) && (item.sendID == uID)){return true;}
+		return false
 	}
 
 
